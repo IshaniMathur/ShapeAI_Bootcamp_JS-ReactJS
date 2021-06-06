d430c60738893d391602a198e32b9c14ba366363
IshaniMathur <85404705+IshaniMathur@users.noreply.github.com>
2021-06-07 00:11:34 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> JavaScript and React.js</h1>
+      <p>
+        This was an amazing 7 days bootcamp organised by Shape AI. Our
+        instructor was Shaurya Sinha sir. He taught us everything from scratch
+        including Javascript and react.js. I had an amazing experience and am
+        looking forward for more such interesting bootcamps.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
